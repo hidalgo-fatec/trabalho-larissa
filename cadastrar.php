bf166da7f6bf0c2f6a1c0bf3e14d8c0b9ecd26d8
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2023-05-18 09:44:46 -0300

    Inserindo o cadastro.php

--- cadastrar.php
+++ cadastrar.php
@@ -99,17 +99,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
         integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous">
     </script>
-    <script>
-    document.addEventListener("DOMContentLoaded", function(e) {
-        let input = document.querySelector('input[autocomplete="off"]');
-     
-        input.setAttribute('disabled', 'disabled');
-     
-        setTimeout(function(){
-           input.removeAttribute('disabled');
-        }, 1000)
-     });
-    </script>
+
 </body>
 
 </html>
